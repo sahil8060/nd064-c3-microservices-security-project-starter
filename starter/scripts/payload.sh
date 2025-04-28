cf51f79aeef6d5518e481d873312547c23f3f7a5
Sahil Goyal <sahilgoyal@Sahils-MacBook-Pro.local>
2025-04-28 11:51:05 +0530

    payload.sh issue fixed

--- starter/scripts/payload.sh
+++ starter/scripts/payload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#start monero_cpu_moneropool
-kubectl run --kubeconfig kube_config_cluster.yml moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
+#start monero_cpu_moneropool:latest
+kubectl create deployment moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
+#start xmrpooldotnet
+kubectl create deployment xmrpooldotnet --image=servethehome/monero_cpu_xmrpooldotnet:latest --replicas=1
 #start minergate
-kubectl run --kubeconfig kube_config_cluster.yml minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
-#start cryptotonight
-kubectl run --kubeconfig kube_config_cluster.yml cryptotonight --image=servethehome/universal_cryptonight:latest --replicas=1
+kubectl create deployment minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
 
 echo "Can you identify the payload(s)?"
